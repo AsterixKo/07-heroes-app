24df279f1d55d71aa106a5b2965c13ed623f4c2d
Javier Moneo <jvr.moneo@gmail.com>
2023-09-28 18:31:58 +0200

    Tarjeta del Heroe parte 2

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+  //   if (alter_ego === characters) return <></>;
+  //   return <p>{characters}</p>;
+  return alter_ego === characters ? <></> : <p>{characters}</p>;
+};
+
 export const HeroCard = ({
   id,
   superhero,
@@ -7,6 +15,9 @@ export const HeroCard = ({
   characters,
 }) => {
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
+
+  //   const charactersByHero = <p>{characters}</p>;
+
   return (
     <div className="col">
       <div className="card">
@@ -18,7 +29,15 @@ export const HeroCard = ({
             <div className="car-body">
               <h5 className="card-title">{superhero}</h5>
               <p className="card-text">{alter_ego}</p>
-              <p>{characters}</p>
+              {/* {alter_ego !== characters && charactersByHero} */}
+
+              <CharactersByHero characters={characters} alter_ego={alter_ego} />
+
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+
+              <Link to={`/hero/${id}`}>Más...</Link>
             </div>
           </div>
         </div>
