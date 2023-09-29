6d51534794d505d0475aee30f231b8504869425e
Javier Moneo <jvr.moneo@gmail.com>
2023-09-29 11:55:10 +0200

    Mostrar mensajes condicionales

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -11,6 +11,9 @@ export const SearchPage = () => {
   // console.log({ q });
   const heroes = getHeroesByName(q);
 
+  const showSearch = q.length === 0;
+  const showError = q.length > 0 && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: q,
   });
@@ -18,9 +21,9 @@ export const SearchPage = () => {
   const onSearchSubmit = (event) => {
     event.preventDefault();
 
-    if (searchText.trim().length <= 1) {
-      return;
-    }
+    // if (searchText.trim().length <= 1) {
+    //   return;
+    // }
     // console.log({ searchText });
     navigate(`?q=${searchText}`);
   };
@@ -51,9 +54,28 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-          <div className="alert alert-primary">Search a hero</div>
 
-          <div className="alert alert-danger">
+          {/* {q === "" ? (
+            <div className="alert alert-primary">Search a hero</div>
+          ) : (
+            heroes.length === 0 && (
+              <div className="alert alert-danger">
+                No hero with <b>{q}</b>
+              </div>
+            )
+          )} */}
+
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? "" : "none" }}
+          >
+            Search a hero
+          </div>
+
+          <div
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? "" : "none" }}
+          >
             No hero with <b>{q}</b>
           </div>
 
