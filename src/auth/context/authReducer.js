204df3c40ceb870bb21443e1c858052b5dde0cc8
Javier Moneo <jvr.moneo@gmail.com>
2023-10-03 17:37:50 +0200

    Pruebas en el authReducer

--- src/auth/context/authReducer.js
+++ src/auth/context/authReducer.js
@@ -5,7 +5,8 @@ export const authReducer = (state = {}, action) => {
     case types.login:
       return { ...state, logged: true, user: action.payload };
     case types.logout:
-      return { ...state, logged: false };
-      defalut: return state;
+      return { logged: false };
+    default:
+      return state;
   }
 };
