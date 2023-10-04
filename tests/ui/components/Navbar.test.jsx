27edd7c5c7a6f9e4eccd226b51e7ec7ebe5b072c
Javier Moneo <jvr.moneo@gmail.com>
2023-10-04 11:53:52 +0200

    Pruebas en el Navbar

---
+++ tests/ui/components/Navbar.test.jsx
@@ -0,0 +1,7 @@
+describe("Pruebas en <Navbar />", () => {
+  test("debe de mostrar el nombre del usuario", () => {
+    
+  });
+
+  test("debe de llamar el logout y navigate cuando se hace click en el boton", () => {});
+});
